dc59fe9fc86a2a13b039314047b1615a2e0b2b1c
Xavier Fabregat <xavi.fabregat.pous@gmail.com>
2022-11-21 12:34:49 +0100

    [FE]Feat: Add get request to form

--- frontend/src/components/Form/userForm.js
+++ frontend/src/components/Form/userForm.js
@@ -6,17 +6,19 @@ export function UserForm ({setUser}) {
 
     async function handleSubmit (e) {
         e.preventDefault();
+        const method = e.target.method.value;
         const userName = e.target.user.value;
         const _id = uuidv4();
-        const userCreatedResponse = await fetch(`http://localhost:5000/user/${_id}`, {
-            method: 'POST',
+        const searchQuery = (method === 'POST') ? _id : userName;
+        const userCreatedResponse = await fetch(`http://localhost:5000/user/${searchQuery}`, {
+            method,
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({
+            body: method === 'POST' ? JSON.stringify({
                 userName,
                 _id,
-            })
+            }) : null
         })
         const userCreated = await userCreatedResponse.json();
         console.log(userCreated)
@@ -26,6 +28,10 @@ export function UserForm ({setUser}) {
     return (
         <form onSubmit={handleSubmit} className='userForm'>
             <input type={'text'} name='user'/>
+            <select name='method'>
+                <option value={'GET'}> GET </option>
+                <option value={'POST'}> POST </option>
+            </select>
             <input type={'submit'}/>
         </form>
     )
